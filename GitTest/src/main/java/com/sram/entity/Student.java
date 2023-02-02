10d93b35ff7901a86bdb33615c856a915e30c376
caiting_P09 <ting.cai@pds-inc.com.cn>
2023-02-02 11:49:30 +0800

    新增student实体类

---
+++ GitTest/src/main/java/com/sram/entity/Student.java
@@ -0,0 +1,12 @@
+package com.sram.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Student {
+    private Integer id;
+    private String username;
+    private Integer age;
+}
